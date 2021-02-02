5ba012328f569786d0e6bef887d578ecddc22226
dnzhngl <65279836+dnzhngl@users.noreply.github.com>
2021-02-02 06:01:56 +0300

    isItSame function revised.

--- get-quote.py
+++ get-quote.py
@@ -7,13 +7,15 @@ def primary():
   last = len(quotes)- 1
   rnd = random.randint(0, last)
   rnd2 = random.randint(0, last)
-  def isItSame():
-    if rnd == rnd2 :
-      rnd2 = random.randint(0, last)
-      isItSame()
-      return rnd2
+  isItSame(rnd, rnd2, last)
     
   print(quotes[rnd],quotes[rnd2], end='')
+  
+def isItSame(rnd1, rnd2, last):
+  if rnd1 == rnd2 :
+    rnd2 = random.randint(0, last)
+    isItSame()
+    return rnd2
 
 def writeQuote():
   quote = input("Enter quote :")
